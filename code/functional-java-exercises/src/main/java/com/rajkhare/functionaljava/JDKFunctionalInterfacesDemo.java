90ef33eee9359bf47f35b60e745a7b4ddc5b68ee
Raj Khare <raj.khare.java@gmail.com>
2026-04-05 06:21:30 +0530

    update for .gitignore

--- code/functional-java-exercises/src/main/java/com/rajkhare/functionaljava/JDKFunctionalInterfacesDemo.java
+++ code/functional-java-exercises/src/main/java/com/rajkhare/functionaljava/JDKFunctionalInterfacesDemo.java
@@ -1,4 +1,29 @@
 package com.rajkhare.functionaljava;
 
+import java.util.function.*;
+
 public class JDKFunctionalInterfacesDemo {
+
+    public static void main(String[] args) {
+
+
+//        Function<Integer, Integer> f1 = x -> x * 2;
+        UnaryOperator<Integer> f1 = x -> x * 2;
+        f1.apply(10);
+
+        Function<Integer, String> f2 = num -> "Value is " + num;
+
+        Consumer<String> f3 = name -> System.out.println("Hello " + name);
+        f3.accept("Raj");
+
+        Supplier<Double> f4 = () -> Math.random();
+        f4.get();
+
+        Predicate<Integer> f5 = num -> (num % 2) == 0;
+        f5.test(10);
+//         Function<Integer, Boolean> f5 = num -> (num % 2) == 0;
+
+
+    }
+
 }
--- code/functional-java-exercises/src/main/java/com/rajkhare/functionaljava/LambdaComposability.java
+++ code/functional-java-exercises/src/main/java/com/rajkhare/functionaljava/LambdaComposability.java
@@ -1,4 +1,21 @@
 package com.rajkhare.functionaljava;
 
+import java.time.LocalDateTime;
+import java.util.function.UnaryOperator;
+
 public class LambdaComposability {
+
+    public static void operationLogger(UnaryOperator<Integer> increment) {
+        System.out.println("Start: " + LocalDateTime.now());
+        increment.apply(10);
+        System.out.println("End: " + LocalDateTime.now());
+    }
+
+    public static void main(String[] args) {
+        UnaryOperator<Integer> increment = x -> x + 1;
+
+        LambdaComposability.operationLogger(increment);
+
+    }
+
 }
