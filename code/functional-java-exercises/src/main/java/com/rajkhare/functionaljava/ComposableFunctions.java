515d9c3ff6a01d74e29116c0e16b3f52c2870780
Raj Khare <raj.khare.java@gmail.com>
2026-04-05 19:23:00 +0530

    Composibility with andThen() and compose()

---
+++ code/functional-java-exercises/src/main/java/com/rajkhare/functionaljava/ComposableFunctions.java
@@ -0,0 +1,36 @@
+package com.rajkhare.functionaljava;
+
+import java.util.function.Function;
+
+public class ComposableFunctions {
+
+    public static void main(String[] args) {
+        Function<Integer, Integer> increment = x -> x + 1;
+        Function<Integer, Integer> doubleIt = x -> x * 2;
+        
+        int i = 10;
+
+        Function<Integer, Integer> combine = doubleIt
+                .andThen(increment)
+                .andThen(doubleIt)
+                .andThen(doubleIt)
+                .andThen(increment);
+
+        System.out.println(combine.apply(i));
+
+        Function<String, String> trimLeading = String::stripLeading;
+        Function<String, String> trimEnding = String::stripTrailing;
+        Function<String, String> upperCase = String::toUpperCase;
+
+        String name = "   rajkhare1    ";
+
+        String processedName = trimLeading
+                .andThen(String::stripTrailing)
+                .andThen(String::toUpperCase)
+                .apply(name);
+
+        System.out.println(processedName);
+
+    }
+
+}
--- code/functional-java-exercises/src/main/java/com/rajkhare/functionaljava/LambdaComposability.java
+++ code/functional-java-exercises/src/main/java/com/rajkhare/functionaljava/LambdaComposability.java
@@ -1,21 +1,34 @@
 package com.rajkhare.functionaljava;
 
 import java.time.LocalDateTime;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 import java.util.function.UnaryOperator;
 
 public class LambdaComposability {
 
-    public static void operationLogger(UnaryOperator<Integer> increment) {
+    public static void operationLogger(UnaryOperator<Integer> operator) {
         System.out.println("Start: " + LocalDateTime.now());
-        increment.apply(10);
+        operator.apply(10);
         System.out.println("End: " + LocalDateTime.now());
     }
 
+
     public static void main(String[] args) {
-        UnaryOperator<Integer> increment = x -> x + 1;
 
-        LambdaComposability.operationLogger(increment);
+        Consumer<String> logMsgWithTimestamp = msg -> System.out.println(msg+": " + LocalDateTime.now());
+        Runnable logStart = () -> logMsgWithTimestamp.accept("Start");
+        Runnable logEnd = () -> logMsgWithTimestamp.accept("End");
 
+        BiConsumer<UnaryOperator<Integer>, Integer> logger = (operation, number) -> {
+            logStart.run();
+            operation.apply(number);
+            logEnd.run();
+
+        };
+
+        logger.accept(x -> x + 1, 20);
+        logger.accept(x -> x * 100, 42);
     }
 
 }
