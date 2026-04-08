3f6c048ebfb814f83e05810ffe908d674b940254
raj.khare@oracle.com <raj.khare@oracle.com>
2026-04-08 16:37:42 +0530

    different ways to create a Stream

--- code/functional-java-exercises/src/main/java/streams/StreamsDemo.java
+++ code/functional-java-exercises/src/main/java/streams/StreamsDemo.java
@@ -1,12 +1,17 @@
 package streams;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 import java.util.Set;
 import java.util.stream.Stream;
 
 public class StreamsDemo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         // Using the stream() API
         Set<String> names = Set.of("Foo", "Bar", "Baz");
@@ -19,6 +24,28 @@ public class StreamsDemo {
 
         // Using Stream.of
         Stream.of("mango", "banana", "strawberry").forEach(System.out::println);
+
+        // Using built in APIs that return Stream
+        Stream<String> lines = Files.lines(Paths.get("README.md"));
+        lines.forEach(System.out::println);
+
+        // Using Stream.generate() : infinite Stream
+        Stream.generate(new Random()::nextDouble)
+                .limit(10)
+                .forEach(System.out::println);
+
+        // Using Stream.iterate(seed, UnaryOperator) : infinite Stream
+        Stream.iterate(0, x -> x + 2)
+                .limit(10)
+                .forEach(System.out::println);
+        
+        // Create collection from the Stream: toList()
+        List<Integer> upToHundred = Stream.iterate(0, x -> x + 1)
+                .limit(100)
+                .toList();
+        System.out.println(upToHundred);
+
+
     }
 
 }
