75385eb5d2b6036c5b41b0176dc85b93033e830b
Raj Khare <raj.khare.java@gmail.com>
2026-04-11 16:51:50 +0530

    completed the course with certification

--- code/functional-java-exercises/src/main/java/streams/IntermediateOperations.java
+++ code/functional-java-exercises/src/main/java/streams/IntermediateOperations.java
@@ -43,6 +43,54 @@ public class IntermediateOperations {
                 .sorted()
                 .forEach(System.out::println);
 
+        System.out.println("-------------------------------------------");
+
+        Stream<Integer> oddNumbers = Stream.of(1, 3, 5, 7, 9);
+        Stream<Integer> evenNumbers = Stream.of(0, 2 , 4, 6, 8);
+        Stream.concat(evenNumbers, oddNumbers).forEach(System.out::println);
+
+        Stream<Student> studentStream = Stream.of(
+                new Student(1, "Foo"),
+                new Student(2, "Bar"),
+                new Student(3, "A")
+
+                );
+
+        System.out.println("-------------------------------------------");
+
+        List<String> studentNames = studentStream.filter(s -> s.getName().length() > 2)
+                .map(s -> s.getName())
+                .toList();
+
+        System.out.println("studentNames = "+studentNames);
+
+
     }
 
+}
+
+class Student {
+    int id;
+    String name;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
